6d79e1f216c3208d25168d38d9df74a508df52f9
Hennadii Zaichenko <zaychenko.gennadiy@gmail.com>
2017-06-01 15:43:38 +0300

    Implement 7.9b: flag for ignore case

--- 7/7.9/tr.py
+++ 7/7.9/tr.py
@@ -13,17 +13,22 @@ translation on. For example, if srcstr == 'abc', dststr == 'mno', and string ==
 For this exercise, you can use the chr() and ord() BIFs, but they are not
 necessary to arrive at a solution.
 '''
-def tr(srcstr, dststr, string):
+def tr(srcstr, dststr, string, icase=False):
   # build a map
   map = {}
   n = min(len(srcstr), len(dststr))
   for i in range(n):
-    map[srcstr[i]] = dststr[i]
+    srcchr = srcstr[i].lower() if icase else srcstr[i]
+    dstchr = dststr[i].lower() if icase else dststr[i]
+    map[srcchr] = dstchr
   # translate a string
   res = []
   for ch in string:
-    mapch = map.get(ch)
+    mapch = map.get(ch.lower() if icase else ch)
     if mapch:
+      if icase:
+        # update case
+        mapch = mapch.upper() if ch.isupper() else mapch.lower()
       res.append(mapch)
     else:
       res.append(ch)
@@ -31,8 +36,12 @@ def tr(srcstr, dststr, string):
   
 def test():
   t = tr('abc', 'mno', 'abcdef')
-  print(t)
   assert t == 'mnodef'
+  t = tr('abc', 'mno', 'AbCdEf')
+  assert t == 'AnCdEf'
+  t = tr('abc', 'mno', 'AbCdEf', True)
+  assert t == 'MnOdEf'
+  print('Checks passed OK')
   
 if __name__ == '__main__':
   test()
