248480eaff9b1d6fa7dc851039b48bb1e529ec6f
Hennadii Z <zaychenko.gennadiy@gmail.com>
2017-06-13 20:02:02 +0300

    Extract code to function

--- 9/9.14/calc.py
+++ 9/9.14/calc.py
@@ -7,30 +7,44 @@ respectively, and displays the result of carrying out that operation on the inpu
 operands. Hint: You may use the string split() method, but you cannot use the exal
 () built-in function.
 '''
-
 import operator
 
-def main():
+class CalcError(Exception):
+    pass
+
+def num_calc(text):
     op_map = {'+':operator.add,
               '-':operator.sub,
               '*':operator.mul,
               '/':operator.truediv,
               '%':operator.mod,
               '**':operator.pow}
+
+    op_list = text.split(' ')
+    if len(op_list) != 3:
+        raise CalcError('Wrong input')
+      
+    n1, op, n2 = op_list
+    if op not in op_map:
+        raise CalcError('Error: Unsupported operator ' + op)
+
+    n1 = int(n1)
+    n2 = int(n2)
+    r = op_map[op](n1, n2)
+    return r
+
+def main():
     while True:
-        text = input()
-        op_list = text.split(' ')
-        if len(op_list) != 3:
-            print('Error: Wrong input')
+        try:
+            text = input()
+            r = num_calc(text)
+            print(r)
+        except EOFError as e:
             break
-        n1, op, n2 = op_list
-        if op not in op_map:
-            print('Error: Unsupported operator', op)
+        except Exception as e:
+            print('Error:', e)
             break
-        n1 = int(n1)
-        n2 = int(n2)
-        r = op_map[op](n1, n2)
-        print(r)
+        
 
 if __name__ == '__main__':
     main()
